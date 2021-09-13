ddc0e8b93d4fe7ec06507597c411d0ebf92a6e1e
ohidurgclan <ohidur.ru.2019@gmail.com>
2021-09-13 07:02:51 +0600

    final ui

--- js/app.js
+++ js/app.js
@@ -9,19 +9,35 @@ const loadProducts = () => {
 const showProducts = (products) => {
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
-    const image = product.images;
+    const image = product.image;
     const div = document.createElement("div");
-    div.classList.add("product");
-    div.innerHTML = `<div class="single-product">
-      <div>
-    <img class="product-image" src=${image}></img>
-      </div>
-      <h3>${product.title}</h3>
-      <p>Category: ${product.category}</p>
-      <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
-      `;
+    div.classList.add("col", "card-border");
+    div.innerHTML = `<div class="card h-100"> <div class="card-image">
+  <img src="${image}" class="mb-2 product-image" alt="..."></div>
+  <div class="card-body">
+    <h5 class="fw-bold card-title">${product.title}</h5>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Category: ${product.category}</li>
+    <li class="list-group-item">Average Rating: ${product.rating.rate} by ${product.rating.count} Customers</li>
+    <li class="list-group-item">Price: $ ${product.price}</li>
+  </ul>
+  <div class="card-body">
+    <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
+      <button id="details-btn" class="btn btn-danger">Details</button>
+  </div>
+</div>`;
+    // <div class="single-product">
+    //   <div>
+    //     <img class="product-image" src=${image}></img>
+    //   </div>
+    //   <h3>${product.title}</h3>
+    //   <p>Category: ${product.category}</p>
+    //   <p>Rating: ${product.rating.rate} / Count: ${product.rating.count}</p>
+    //   <h2>Price: $ ${product.price}</h2>
+    //   <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
+    //   <button id="details-btn" class="btn btn-danger">Details</button></div>
+
     document.getElementById("all-products").appendChild(div);
   }
 };
@@ -45,7 +61,7 @@ const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+  document.getElementById(id).innerText = Math.round(total.toFixed(2));
 };
 
 // set innerText function
